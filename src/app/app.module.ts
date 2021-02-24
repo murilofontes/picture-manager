ea3f8c21ca18ba8b1a9cb39a1f2e6f69fe823931
murilo_fontes <murilo.fontes@dasa.com.br>
2021-02-23 21:34:15 -0300

    Inicio de projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { parseHostBindings } from '@angular/compiler';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PhotoComponent } from './photo/photo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PhotoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
